65acd063c7eae8a132a75ed5e061f0f6b2c14bbf
Louis Lescure <louis.lescure@mail.mcgill.ca>
2019-11-12 16:56:37 +0100

    hmm

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times {
+  article = Article.new(title: "#{Faker::DcComics.hero}, #{Faker::Hipster.word} and #{Faker::Hipster.word}", content: Faker::Hipster.paragraph(sentence_count: 25))
+  article.save
+}
